7c9b7ad20dc439880515bf1a6e77ba88269e43f7
Alan Accurso <accurso.alan@gmail.com>
2017-11-27 18:46:55 -0300

    Configure Firebase with CocoaPods

--- App.js
+++ App.js
@@ -13,10 +13,14 @@ import {
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: `
+    Press Cmd+R to reload,
+    Cmd+D or shake for dev menu
+  `,
+  android: `
+    Double tap R on your keyboard to reload,
+    Shake or press menu button for dev menu
+  `,
 });
 
 export default class App extends Component<{}> {
