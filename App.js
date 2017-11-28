6d875e9e839354ca97ea815b3e7a488b9298da7e
Alan Accurso <accurso.alan@gmail.com>
2017-11-28 10:37:38 -0300

    Add Firebase/Messaging

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Room Booking Valtech!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
--- ios/RoomBookingValtech/AppDelegate.m
+++ ios/RoomBookingValtech/AppDelegate.m
@@ -33,7 +33,33 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  // Firebase INIT
   [FIRApp configure];
+  
+  // Firebase Messaging
+  if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+    UIUserNotificationType allNotificationTypes =
+    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    UIUserNotificationSettings *settings =
+    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+    [application registerUserNotificationSettings:settings];
+  } else {
+    // iOS 10 or later
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    // For iOS 10 display notification (sent via APNS)
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    UNAuthorizationOptions authOptions =
+    UNAuthorizationOptionAlert
+    | UNAuthorizationOptionSound
+    | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    }];
+#endif
+  }
+  
+  [application registerForRemoteNotifications];
+  
   return YES;
 }
 
